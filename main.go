fe048855ff1bf6a3d1b0b579a06a707372d7bb34
Krisztian Godrei <godreikrisztian@gmail.com>
2016-10-21 18:01:57 +0200

    in progress

--- main.go
+++ main.go
@@ -1,124 +1,139 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 	"os"
+	"strings"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
+	"google.golang.org/api/androidpublisher/v2"
+	"google.golang.org/api/googleapi"
 
 	"github.com/bitrise-io/go-utils/fileutil"
+	"github.com/bitrise-io/go-utils/log"
 )
 
 func main() {
+	//
+	// Configs
 	jsonKeyPth := "/Users/godrei/Downloads/key.json"
-	packageName := "com.godrei.multiplatform"
-	apkPath := "/Users/godrei/Downloads/com.godrei.multiplatform.apk"
+	packageName := "com.multipleabitest.godrei.multipleabitest"
+	apkPath := "/Users/godrei/Downloads/apks/app-armeabi-v7a-release.apk|/Users/godrei/Downloads/apks/app-mips-release.apk|/Users/godrei/Downloads/apks/app-x86-release.apk"
+	trackName := "beta"
+	//
 
 	//
 	// Create client
+	fmt.Println()
+	log.Info("Create client")
+
 	jsonKeyBytes, err := fileutil.ReadBytesFromFile(jsonKeyPth)
 	if err != nil {
-		log.Fatalf("Failed to read json key, error: %s", err)
+		log.Error("Failed to read json key, error: %s", err)
 		os.Exit(1)
 	}
 
 	config, err := google.JWTConfigFromJSON(jsonKeyBytes, "https://www.googleapis.com/auth/androidpublisher")
 	if err != nil {
-		log.Fatalf("Failed to create config, error: %s", err)
+		log.Error("Failed to create config, error: %s", err)
 		os.Exit(1)
 	}
 
-	fmt.Println()
-	fmt.Printf("config.Email: %s\n", config.Email)
-	// fmt.Printf("config.PrivateKey: %s\n", config.PrivateKey)
-	fmt.Printf("config.Scopes: %s\n", config.Scopes)
-	fmt.Printf("config.TokenURL: %s\n", config.TokenURL)
+	log.Detail(" config.Email: %s", config.Email)
+	log.Detail(" config.Scopes: %s", config.Scopes)
+	log.Detail(" config.TokenURL: %s", config.TokenURL)
 
 	client := config.Client(oauth2.NoContext)
+	service, err := androidpublisher.New(client)
+	if err != nil {
+		log.Error("Failed to create publisher service, error: %s", err)
+		os.Exit(1)
+	}
 	// ---
 
 	//
 	// Create insert edit
-	insertEditURL := fmt.Sprintf("https://www.googleapis.com/androidpublisher/v2/applications/%s/edits", packageName)
-	request, err := http.NewRequest("POST", insertEditURL, nil)
-	if err != nil {
-		log.Fatalf("Failed to create insert edit request, error: %s", err)
-		os.Exit(1)
-	}
-
-	response, err := client.Do(request)
-	if err != nil {
-		log.Fatalf("Failed to perform request, error: %s", err)
-		os.Exit(1)
-	}
-	defer response.Body.Close()
-
 	fmt.Println()
-	fmt.Printf("response status: %d\n", response.StatusCode)
+	log.Info("Create new edit")
 
-	if response.StatusCode != 200 {
-		log.Fatalf("Non success status code: %d", response.StatusCode)
-		os.Exit(1)
-	}
+	editsService := androidpublisher.NewEditsService(service)
 
-	body, err := ioutil.ReadAll(response.Body)
+	editsInsertCall := editsService.Insert(packageName, nil)
+
+	appEdit, err := editsInsertCall.Do()
 	if err != nil {
-		log.Fatalf("Failed to read response, error: %s", err)
+		log.Error("Failed to perform edit insert call, error: %s", err)
 		os.Exit(1)
 	}
 
-	var respJSON map[string]interface{}
-	if err := json.Unmarshal(body, &respJSON); err != nil {
-		log.Fatalf("Failed to marshal response, error: %s", err)
-		os.Exit(1)
-	}
-
-	editID := respJSON["id"]
-
-	fmt.Println()
-	fmt.Printf("editID: %s\n", editID)
+	log.Detail(" editID: %s", appEdit.Id)
 	// ---
 
 	//
-	// Upload APK
-	fileUploadURL := fmt.Sprintf("https://www.googleapis.com/upload/androidpublisher/v2/applications/%s/edits/%s/apks?uploadType=media", packageName, editID)
-	apkFile, err := os.Open(apkPath)
+	// Upload APKs
+	fmt.Println()
+	log.Info("Edit apks upload")
+
+	versionCode := []int64{}
+	apkPaths := strings.Split(apkPath, "|")
+	for _, apkPath := range apkPaths {
+		apkFile, err := os.Open(apkPath)
+		if err != nil {
+			log.Error("Failed to read apk (%s), error: %s", apkPath, err)
+			os.Exit(1)
+		}
+
+		editsApksService := androidpublisher.NewEditsApksService(service)
+
+		editsApksUloadCall := editsApksService.Upload(packageName, appEdit.Id)
+		editsApksUloadCall.Media(apkFile, googleapi.ContentType("application/vnd.android.package-archive"))
+
+		apk, err := editsApksUloadCall.Do()
+		if err != nil {
+			log.Error("Failed to upload apk, error: %s", err)
+			os.Exit(1)
+		}
+
+		log.Detail(" version: %d", apk.VersionCode)
+		versionCode = append(versionCode, apk.VersionCode)
+	}
+	// ---
+
+	//
+	// Update track
+	fmt.Println()
+	log.Info("Update track")
+
+	editsTracksService := androidpublisher.NewEditsTracksService(service)
+
+	newTrack := androidpublisher.Track{
+		Track:        trackName,
+		UserFraction: 1.0,
+		VersionCodes: versionCode,
+	}
+
+	editsTracksUpdateCall := editsTracksService.Update(packageName, appEdit.Id, trackName, &newTrack)
+	track, err := editsTracksUpdateCall.Do()
 	if err != nil {
-		log.Fatalf("Failed to read apk (%s), error: %s", apkPath, err)
+		log.Error("Failed to update track, error: %s", err)
 		os.Exit(1)
 	}
 
-	request, err = http.NewRequest("POST", fileUploadURL, apkFile)
-	if err != nil {
-		log.Fatalf("Failed to create upload request, error: %s", err)
-		os.Exit(1)
-	}
-	request.Header.Set("content-type", "application/vnd.android.package-archive")
+	log.Detail(" track: %s", track.Track)
+	log.Detail(" versions: %s", track.VersionCodes)
+	// ---
 
-	response, err = client.Do(request)
+	//
+	// Commit edit
+	editsCommitCall := editsService.Commit(packageName, appEdit.Id)
+	appEdit, err = editsCommitCall.Do()
 	if err != nil {
-		log.Fatalf("Failed to perform request, error: %s", err)
+		log.Error("Failed to commit edit (%s), error: %s", appEdit.Id, err)
 		os.Exit(1)
 	}
-	defer response.Body.Close()
 
 	fmt.Println()
-	fmt.Printf("response status: %d\n", response.StatusCode)
-
-	if response.StatusCode != 200 {
-		log.Fatalf("Non success status code: %d", response.StatusCode)
-		os.Exit(1)
-	}
-	body, err = ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatalf("Failed to read response, error: %s", err)
-		os.Exit(1)
-	}
-	fmt.Printf("%s\n", string(body))
+	log.Done("Edit committed")
 	// ---
 }
