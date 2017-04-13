acf30af64f1422d92c76ef7fd656f5c0eca80ce2
Tamas Papik <trapacska@gmail.com>
2017-04-13 13:43:42 +0200

    Changed test

--- main_test.go
+++ main_test.go
@@ -6,65 +6,65 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGetKeyPath(t *testing.T) {
+func TestParseKeyPath(t *testing.T) {
 
-	t.Log("GetKeyPath - file://../../../../../../Downloads/key.json")
+	t.Log("prepareKeyPath - file://../../../../../../Downloads/key.json")
 	{
-		keyPth, isRemote, err := getKeyPath("file://../../../../../../Downloads/key.json")
+		keyPth, isRemote, err := prepareKeyPath("file://../../../../../../Downloads/key.json")
 		require.NoError(t, err)
 
 		require.Equal(t, "../../../../../../Downloads/key.json", keyPth)
 		require.Equal(t, false, isRemote)
 	}
 
-	t.Log("GetKeyPath - file://./")
+	t.Log("prepareKeyPath - file://./")
 	{
-		keyPth, isRemote, err := getKeyPath("file://./testfolder/key.json")
+		keyPth, isRemote, err := prepareKeyPath("file://./testfolder/key.json")
 		require.NoError(t, err)
 
 		require.Equal(t, "./testfolder/key.json", keyPth)
 		require.Equal(t, false, isRemote)
 	}
 
-	t.Log("GetKeyPath - file:///")
+	t.Log("prepareKeyPath - file:///")
 	{
-		keyPth, isRemote, err := getKeyPath("file:///testfolder/key.json")
+		keyPth, isRemote, err := prepareKeyPath("file:///testfolder/key.json")
 		require.NoError(t, err)
 
 		require.Equal(t, "/testfolder/key.json", keyPth)
 		require.Equal(t, false, isRemote)
 	}
 
-	t.Log("GetKeyPath - http://")
+	t.Log("prepareKeyPath - http://")
 	{
-		keyPth, isRemote, err := getKeyPath("http://testdomain.com/testsub/key.json")
+		keyPth, isRemote, err := prepareKeyPath("http://testdomain.com/testsub/key.json")
 		require.NoError(t, err)
 
 		require.Equal(t, "http://testdomain.com/testsub/key.json", keyPth)
 		require.Equal(t, true, isRemote)
 	}
 
-	t.Log("GetKeyPath - https://")
+	t.Log("prepareKeyPath - https://")
 	{
-		keyPth, isRemote, err := getKeyPath("https://testdomain.com/testsub/key.json")
+		keyPth, isRemote, err := prepareKeyPath("https://testdomain.com/testsub/key.json")
 		require.NoError(t, err)
 
 		require.Equal(t, "https://testdomain.com/testsub/key.json", keyPth)
 		require.Equal(t, true, isRemote)
 	}
 
-	t.Log("GetKeyPath - ./")
+	t.Log("prepareKeyPath - ./")
 	{
-		keyPth, isRemote, err := getKeyPath("./user/test/key.json")
+		keyPth, isRemote, err := prepareKeyPath("./user/test/key.json")
 		require.NoError(t, err)
 
 		require.Equal(t, "./user/test/key.json", keyPth)
 		require.Equal(t, false, isRemote)
 	}
 
-	t.Log("GetKeyPath - /")
+	t.Log("prepareKeyPath - /")
 	{
-		keyPth, isRemote, err := getKeyPath("/user/test/key.json")
+		keyPth, isRemote, err := prepareKeyPath("/user/test/key.json")
 		require.NoError(t, err)
 
 		require.Equal(t, "/user/test/key.json", keyPth)
