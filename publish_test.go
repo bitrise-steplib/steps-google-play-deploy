5d3b7d71d6d1a7529ec7b14f08cd64771583a89b
Ale de la Viña <alejandro.delavina@gruposantander.com>
2020-07-28 15:17:25 +0200

    Corrected test

--- publish_test.go
+++ publish_test.go
@@ -20,7 +20,7 @@ func Test_releaseStatusFromConfig(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := releaseStatusFromConfig(tt.userFraction); got != tt.want {
+			if got := releaseStatusFromConfig(tt.userFraction, false); got != tt.want {
 				t.Errorf("releaseStatusFromConfig() = %v, want %v", got, tt.want)
 			}
 		})
