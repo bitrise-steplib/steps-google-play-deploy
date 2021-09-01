61704dd1eda254538251f1d8098d3953ced7924e
Bence Zsigmond Nagy <nbzs1001@gmail.com>
2021-09-01 14:42:39 +0200

    Generalize error message

--- main.go
+++ main.go
@@ -196,7 +196,7 @@ func executeEdit(service *androidpublisher.Service, configs Configs, changesNotS
 	log.Infof("Upload apks or app bundles")
 	versionCodes, err := uploadApplications(configs, service, appEdit)
 	if err != nil {
-		return fmt.Sprintf("Failed to upload APKs: %v", err)
+		return fmt.Sprintf("Failed to upload application(s): %v", err)
 	}
 	log.Donef("Applications uploaded")
 
